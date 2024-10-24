be3047d0954c85f0a7050af0e793f0e0cec1b183
flevinton <fe.levinton@gmail.com>
2024-10-24 09:33:16 +0200

    rien

--- 4pipeline_create_npy.py
+++ 4pipeline_create_npy.py
@@ -8,10 +8,13 @@ from spike_sorting import *
 sr = 30e3
 t_pre = 0.2#0.2
 t_post = 0.50#0.300
-bin_width = 0.005
+bin_width = 0.02
 psth_bins = np.arange(-t_pre, t_post + bin_width, bin_width)  
 
-path = '/auto/data2/eTheremin/MUROLS/MUROLS_20230218/MUROLS_20230218_SESSION_01/'
+
+session = 'ALTAI_20240724_SESSION_01'
+path = '/Volumes/data2/eTheremin/ALTAI/'+ session + '/'
+#path = '/auto/data2/eTheremin/MUROLS/MUROLS_20230218/MUROLS_20230218_SESSION_01/'
 mock=False
 #session_type = get_session_type_final(path)
 #print(session_type)
